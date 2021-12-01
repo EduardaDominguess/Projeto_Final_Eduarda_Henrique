19834e7a95487123b4df2ffc1c2b4cee09005dbd
HenriqueKrepel <92476696+HenriqueKrepel@users.noreply.github.com>
2021-12-01 10:37:41 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,210 +1,186 @@
 import pygame
-import random
+import random import randint
 import os 
 
+WIDTH = 650
+HEIGHT = 700
+
 '''CLASSES'''
-#classe sapo
-class Sapo:
-    def __init__(self, posicao):
-        self.posicaox = posicao[0]
-        self.posicaoy = posicao[1]
-        self.retangulo = pygame.Rect((self.posicaox, self.posicaoy), (128,128))
-    def sapo_sobe(self):
-        self.posicaoy -= 128
-        self.retangulo.top = self.posicaoy     
-    def sapo_desce(self):
-        self.posicaoy += 128
-        self.retangulo.top = self.posicaoy 
-    def sapo_direita(self):
-        self.posicaox += 128
-        self.retangulo.left = self.posicaox 
-    def sapo_esquerda(self):
-        self.posicaox -= 128
-        self.retangulo.left = self.posicaox 
-#vida      
-class Vidas:
-    def __init__(self,posicao):
-        self.posicaox = posicao[0]
-        self.posicaoy = posicao[y]
-
-#moeda
-class Moeda:
-    def __init__(self, posicao):
-        self.posicaox = posicao[0]
-        self.posicaoy = posicao[1]
-
-#lista carros
-carros_left = ['carro1', 'carro2','carro3']
-carros_right = ['carro4', 'carro5','carro6']
-
-#listas ruas
-#COMPLETAR COM O TAMANHO
-r1 = [275.5, 217]
-r2 = [-285, 42.7]
-r3 = [406.3, -44.5]
-r4 = [-285, -138.3]
-r5 = [283.4, -219.2]
-
-class Carros:
-    def __init__(self,rua, velocidade):
-        self.velocidade = velocidade
-        self.rua = rua
-        if  self.rua == 1:
-            self.posicaox = r1[0]
-            self.posicaoy = r1[1]
-    
-        elif self.rua == 2:
-            self.posicaox = r2[0]
-            self.posicaoy = r2[1]
-            
-        elif self.rua == 3:
-            self.posicaox = r3[0]
-            self.posicaoy = r3[1]
-                
-        elif self.rua == 4:
-            self.posicaox = r4[0]
-            self.posicaoy = r4[1]
-                
-        elif self.rua == 5:
-            self.posicaox = r5[0]
-            self.posicaoy = r5[1]
-
-        if self.rua == 1 or self.rua == 3 or self.rua == 5:
-            sprite = random.choice(carros_right)
-            if sprite == 'carro1':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro1')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 170)) #VER POSICAOOO
-            if sprite == 'carro2':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro2')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 212)) #VER POSICAOOO
-            if sprite == 'carro3':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro3')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 212)) #VER POSICAOOO
-            
-        if self.rua == 2 or self.rua == 4:
-            if sprite == 'carro4':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro4')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 212)) #VER POSICAOOO
-            if sprite == 'carro5':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro5')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 212)) #VER POSICAOOO
-            if sprite == 'carro6':
-                self.imagem = pygame.image.load(os.path.join('Imagens','carro6')).convert_alpha()
-                self.rectangle = pygame.Rect((self.posicaox, self.posicaoy), (128, 212)) #VER POSICAOOO
-    
-    #movimentaçao
-    def movimento(self):
-        if self.rua == 1 or self.rua == 3 or self.rua == 5: 
-            self.posicaoy -= self.velocidade
-            self.retangulo.top = self.posicaoy 
-        elif self.rua == 2 or self.rua == 4 or self.rua == 6: 
-            self.posicaoy += self.velocidade
-            self.retangulo.top = self.posicaoy 
-       
-'''RUAS'''
-#RUA1
-class Rua1:
-    def __init__(self,velocidade):
-        self.velocidade = velocidade
-        self.carro = Carros(1, velocidade)
-        self.carro.movimento()
-
-class Rua2:
-    def __init__(self,velocidade):
-        self.velocidade = velocidade
-        self.carro = Carros(1, velocidade)
-        self.carro.movimento()
-
-class Rua3:
-    def __init__(self,velocidade):
-        self.velocidade = velocidade
-        self.carro = Carros(1, velocidade)
-        self.carro.movimento()
-
-class Rua4:
-    def __init__(self,velocidade):
-        self.velocidade = velocidade
-        self.carro = Carros(1, velocidade)
-        self.carro.movimento()
-
-class Rua5:
-    def __init__(self,velocidade):
-        self.velocidade = velocidade
-        self.carro = Carros(1, velocidade)
-        self.carro.movimento()
-
-
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" #classe sapo
-class Sapos1 (pygame.sprite.Sprite):
+class Frog(sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.Sapo1 = []
-        self.Sapo1.append(pygame.image.load('Sapo1/SAPO1.1.png'))
-        self.Sapo1.append(pygame.image.load('Sapo1/SAPO1.2.png'))
-        self.Sapo1.append(pygame.image.load('Sapo1/SAPO1.3.png'))
-        self.Sapo1.append(pygame.image.load('Sapo1/SAPO1.4.png'))
-        self.atual = 0
-        self.image = self.Sapo1[self.atual]
-        self.image = pygame.transform.scale(self.image, (540/10, 540/10))
-        self.rect = self.image.get_rect() 
-        self.rect.topleft = 100, 100
-    
+        super().__init__()
+        self.x = WIDTH/2
+        self.y = HEIGHT
+        self.width = 100
+        self.height = 100
+
+        self.imageUp = pygame.image.load('frog-up.png')
+        self.imageDown = pygame.image.load('frog-down.png')
+        self.imageRight = pygame.image.load('frog-right.png')
+        self.imageLeft = pygame.image.load('frog-left.png')
+        self.imageUp = pygame.transform.scale(self.imageUp, (self.width, self.height))
+        self.imageDown = pygame.transform.scale(self.imageDown, (self.width, self.height))
+        self.imageRight = pygame.transform.scale(self.imageRight, (self.width, self.height))
+        self.imageLeft = pygame.transform.scale(self.imageLeft, (self.width, self.height))
+
+        self.image = self.imageUp
+        self.rect = self.image.get_rect()
+
     def update(self):
-        if pygame.key.get_pressed()[K_LEFT]: #esquerda
-            self.image = self.Sapo1[3]
-            self.image = pygame.transform.scale(self.image, (540/10, 540/10))
-        if pygame.key.get_pressed()[K_RIGHT]: #direita
-            self.image = self.Sapo1[1]
-            self.image = pygame.transform.scale(self.image, (540/10, 540/10))
-        if pygame.key.get_pressed()[K_UP]: #cima
-            self.image = self.Sapo1[0]
-            self.image = pygame.transform.scale(self.image, (540/10, 540/10))
-        if pygame.key.get_pressed()[K_DOWN]: #baixo
-            self.image = self.Sapo1[2]
-            self.image = pygame.transform.scale(self.image, (540/10, 540/10))
+        self.movement_up()
+        self.movement_down()
+        self.movement_left()
+        self.movement_right()
+        self.correction()
+        self.rect.center = (self.x, self.y)
+
+    def reset_pos(self):
+        self.x = WIDTH/2
+        self.y = HEIGHT
+
+    def movement_left(self):
+        self.x -= 85
+        
+    def movement_right(self):
+        self.x += 85
+
+    def movement_up(self):
+        self.y -= 86
+
+    def movement_down(self):
+        self.y += 86 
+       
+    def correction(self):
+        if self.x - self.width / 2 < 0:
+            self.x = self.width / 2
+        elif self.x + self.width / 2 > WIDTH:
+            self.x = WIDTH - self.width / 2
+
+        if self.y - self.height / 2 < 0:
+            self.y = self.height / 2
+        elif self.y + self.height / 2 > HEIGHT:
+            self.y = HEIGHT - self.height / 2
+
+class Car(sprite.Sprite):
+    def __init__(self, number):
+        super().__init__()
+        if number == 1:
+            self.y = 130
+            self.image = pygame.image.load('car-1.png')
+            self.velocity = 5.1
+        elif number == 2:
+            self.y = 220
+            self.image = pygame.image.load('car-2.png')
+            self.velocity = -3.5
+        elif number == 3:
+            self.y = 307
+            self.image = pygame.image.load('car-3.png')
+            self.velocity = 3
+        elif number == 4:
+            self.y = 395
+            self.image = pygame.image.load('car-4.png')
+            self.velocity = -4
+        else:
+            self.y = 570
+            self.image = pygame.image.load('car-6.png')
+            self.velocity = -3.2
+
+        self.x = WIDTH / 2
+        self.width = 80
+        self.height = 80
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.movement()
+        self.rect.center = (self.x, self.y)
+
+    def movement(self):
+        self.x += self.velocity
+
+        if self.x + self.width / 2 < 0:
+            self.x = WIDTH + self.width / 2
+
+        elif self.x - self.width / 2 > WIDTH:
+            self.x = - self.width / 2
+
+class Coin(sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.x = randint(0, WIDTH)
+        self.y = randint(85, HEIGHT)
+        self.width = 56
+        self.height = 56
+
+        self.image = pygame.image.load('coin.png')
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = (self.x, self.y)
+
+    def reappear(self):
+        self.x = randint(0, WIDTH)
+        self.y = randint(85, HEIGHT)
+
+class Life(sprite.Sprite):
+    def __init__(self, count):
+        super().__init__()
+        self.width = 56
+        self.height = 56
+
+        self.x = self.width * count * 1.1
+        self.y = self.height - 10
+
+        self.image = pygame.image.load('heart.png')
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = (self.x, self.y)
+
+
+class Background(sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.background = pygame.image.load('background.png')
+        self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
+
+        self.image = self.background
+        self.x = 0
+        self.y = 0
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.topleft = (self.x, self.y)
     
-    def move(self, movement):
-        def __init__(self,movement):
-            self.movementx = movement[0]
-            self.movementy = movement[1]
-            self.Sapo1 = pygame.Rect((self.movementx, self.movementy), (128,128))
-        def sobe_raposa(self):
-            self.movementy -= 128
-            self.Sapo1.top = self.movementy     
-        def desce_raposa(self):
-            self.movementy += 128
-            self.Sapo1.top = self.movementy 
-        def direita(self):
-            self.movementx += 128
-            self.Sapo1.left = self.movementx 
-        def esquerda(self):
-            self.movementx -= 128
-            self.Sapo1.left = self.movementx 
+class StartScreen(sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.background = pygame.image.load('start-screen.png')
+        self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
 
-#lista carros
-carros_left = [carro1, carro2, carro3]
-carros_right = [carro4, carro5, carro6]    
+        self.image = self.background
+        self.x = 0
+        self.y = 0
 
-#carros
-class Carros:
-    def __init__(self,rua,velocidade):
-        self.velocidade = velocidade 
-        self.rua = rua
-        if self.rua = 1:
-            
-        pass """
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.topleft = (self.x, self.y)
+
+class EndScreen(sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.background = pygame.image.load('end-screen.png')
+        self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
+
+        self.image = self.background
+        self.x = 0
+        self.y = 0
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.topleft = (self.x, self.y)
